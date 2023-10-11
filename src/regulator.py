593f8427604c3728cfbd4aee73aca0e86ea8e81e
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-10-11 17:56:00 -0300

    feat: implementação da func de remover e refat do insert regulator

--- src/regulator.py
+++ src/regulator.py
@@ -15,21 +15,21 @@ class Regulator:
     def insert_regulator(self, word: str):
         length = len(word)
         if length <= 5:
-            print(self.insert(word, self.lista_1))
+            self.insert(word, self.lista_1)
 
         elif 5 < length <= 10:
-            print(self.insert(word, self.lista_2))
+            self.insert(word, self.lista_2)
         
         else:
-            print(self.insert(word, self.lista_3))
+            self.insert(word, self.lista_3)
         
     def insert(self, word: str, lista: Lista):
         res, node = lista.basic_insert(word)
         if res == False:
-            return f'palavra ja existente: {word}'
+            return print(f'palavra ja existente: {word}')
         
         res, _ = self.lista_4.general_insert(node)
-        return f'palavra inserida: {word}'
+        return print(f'palavra inserida: {word}')
     
     def lister_general_regulator(self, lista: int):
         match lista:
@@ -62,3 +62,20 @@ class Regulator:
         copy_list = copy.deepcopy(self.lista_4)
         self.sort.sort_list(copy_list, 'general')
         copy_list.display_by_alphabetic(init, end)
+
+    def remove_regulator(self, word: str):
+        length = len(word)
+        if length <= 5:
+            self.remove(word, self.lista_1)
+        elif 5 < length < 10:
+            self.remove(word, self.lista_2)
+        else:
+            self.remove(word, self.lista_3)
+    
+    def remove(self, word: str, lista: Lista):
+        res = lista.basic_remove(word)
+        if not res:
+            return print(f'palavra inexistente: {word}')
+
+        res = self.lista_4.general_remove(word)
+        return print(f'palavra removida: {word}')
